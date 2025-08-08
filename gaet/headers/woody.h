1812bf73b350b2ada9fbdb18e0d0c4e713bf24c9
Gaetan Maniquet <gmanique@f2r7s14.clusters.42paris.fr>
2025-08-08 19:17:23 +0200

    Makefile Du seigneur

---
+++ gaet/headers/woody.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   woody.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmanique <gmanique@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/08 19:02:15 by gmanique          #+#    #+#             */
+/*   Updated: 2025/08/08 19:15:24 by gmanique         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WOODY_H
+# define WOODY_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+
+void	do_nothing();
+
+#endif
